3edb54710c6d3be5f49bae6e8887b865165e896a
Wijayanto <wijayanto.a.p@gmail.com>
2024-06-03 14:00:48 +0800

    Handle delete workspace

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -4,6 +4,7 @@ const colors = {
   lightgray: '#F3F6F8',
   darkgray: '#808E9A',
   whitegray: '#FBFCFD',
+  lightred: '#ef5350',
 };
 
 export default colors;
--- src/pages/Workspace.tsx
+++ src/pages/Workspace.tsx
@@ -1,18 +1,21 @@
 import { ChangeEvent, useEffect, useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import { Player } from '@lottiefiles/react-lottie-player';
-import { Box, Button, Container, Grid, Typography } from '@mui/material';
+import { Box, Button, Container, Grid, IconButton, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import FileOpenOutlinedIcon from '@mui/icons-material/FileOpenOutlined';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import loader from '../assets/loader.json';
+import DeleteIcon from '@mui/icons-material/Delete';
 import Header from '../components/Header';
 import useSession from '../hooks/useSession';
 import supabase from '../client/supabase';
 import { LottieJson } from './Editor';
 import { formatDate } from '../helper/dateHelper';
 import colors from '../constants/colors';
+import { useDispatch } from 'react-redux';
+import { toggleBackdrop } from '../redux/slices/overlaySlice';
 
 export interface WorkspaceData {
   id?: string;
@@ -38,6 +41,7 @@ function Workspace() {
   const { data, loading } = useSession();
   const [projects, setProjects] = useState<WorkspaceData[]>([]);
   const [loadingProjects, setLoadingProjects] = useState(true);
+  const dispatch = useDispatch();
 
   const handleFileUpload = async (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -53,6 +57,13 @@ function Workspace() {
       }
     };
     reader.readAsText(file);
+  };
+
+  const handleDeleteWorkspace = async (id: string, index: number) => {
+    dispatch(toggleBackdrop());
+    const response = await supabase.from('workspaces').delete().eq('id', id);
+    if (response) dispatch(toggleBackdrop());
+    setProjects((prevState) => prevState.filter((_, idx) => idx !== index));
   };
 
   const saveWorkspace = async (lottieObj: any) => {
@@ -128,49 +139,63 @@ function Workspace() {
             {loadingProjects && <Player autoplay loop src={loader} />}
             <Grid container spacing={2} sx={{ flex: 1, my: 2, justifyContent: { xs: 'center', sm: 'flex-start' } }}>
               {projects?.map((item, index) => (
-                <Grid item key={index} component={Link} to={`/workspace/${item?.id}`} sx={{ textDecoration: 'none' }}>
-                  <Box
-                    sx={{
-                      width: 214,
-                      height: 220,
-                      backgroundColor: 'white',
-                      cursor: 'pointer',
-                      border: '1px solid #D8E0E5',
-                      borderRadius: 2,
-                      mb: 2,
-                      '&:hover': {
-                        boxShadow: 8,
-                      },
-                    }}
-                  >
-                    <Player autoplay loop src={item?.lottieObj} style={{ width: '100%', height: 158, paddingBottom: 50 }} />
-                    <Box sx={{ p: 2, display: 'flex', flexDirection: 'row', alignItems: 'center', mt: '-50px' }}>
-                      <FileOpenOutlinedIcon sx={{ color: 'primary.main' }} />
-                      <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: '80%', ml: 1 }}>
-                        <Typography
-                          variant="subtitle2"
-                          sx={{
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            whiteSpace: 'nowrap',
-                            width: '100%',
-                            color: 'black',
+                <Grid item key={index}>
+                  <Box component={Link} to={`/workspace/${item?.id}`} sx={{ textDecoration: 'none' }}>
+                    <Box
+                      sx={{
+                        width: 214,
+                        backgroundColor: 'white',
+                        cursor: 'pointer',
+                        border: '1px solid #D8E0E5',
+                        borderRadius: 2,
+                        mb: 2,
+                        '&:hover': {
+                          boxShadow: 8,
+                        },
+                      }}
+                    >
+                      <Box sx={{ display: 'flex' }}>
+                        <Box sx={{ flex: 1 }} />
+                        <IconButton
+                          aria-label="delete"
+                          onClick={(event) => {
+                            event.preventDefault();
+                            event.stopPropagation();
+                            handleDeleteWorkspace(item?.id || '', index);
                           }}
                         >
-                          {item?.name}
-                        </Typography>
-                        <Typography
-                          variant="caption"
-                          sx={{
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            whiteSpace: 'nowrap',
-                            width: '100%',
-                            color: 'gray',
-                          }}
-                        >
-                          Last modified {formatDate(item?.dateModified)}
-                        </Typography>
+                          <DeleteIcon sx={{ color: colors.lightred }} />
+                        </IconButton>
+                      </Box>
+                      <Player autoplay loop src={item?.lottieObj} style={{ width: '100%', height: 158 }} />
+                      <Box sx={{ p: 2, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <FileOpenOutlinedIcon sx={{ color: 'primary.main' }} />
+                        <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: '80%', ml: 1 }}>
+                          <Typography
+                            variant="subtitle2"
+                            sx={{
+                              overflow: 'hidden',
+                              textOverflow: 'ellipsis',
+                              whiteSpace: 'nowrap',
+                              width: '100%',
+                              color: 'black',
+                            }}
+                          >
+                            {item?.name}
+                          </Typography>
+                          <Typography
+                            variant="caption"
+                            sx={{
+                              overflow: 'hidden',
+                              textOverflow: 'ellipsis',
+                              whiteSpace: 'nowrap',
+                              width: '100%',
+                              color: 'gray',
+                            }}
+                          >
+                            Last modified {formatDate(item?.dateModified)}
+                          </Typography>
+                        </Box>
                       </Box>
                     </Box>
                   </Box>
